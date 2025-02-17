055a8a7da6c3e6ab39b04b48d36035d07017a89a
faisalm29 <faisal.muhammad2911@gmail.com>
2025-02-17 15:39:01 +0700

    fixing production error by deleting quote and apostrophe on src/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <h1 className={styles.h1}>
         Hello, World! ini adalah budi 0123456789 rt tt ff yang hilang tumbuh,
         yang patah berganti Rasakan itu Quontol →↗↖↙↘ Illumination Difficult
-        Difftent rf kt Geist It's fun "gelo"
+        Difftent rf kt Geist
       </h1>
     </div>
   );
