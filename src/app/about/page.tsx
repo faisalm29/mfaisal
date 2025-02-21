18b4836222decc5744fe8e87d5b80ef7fb9cdaf4
faisalm29 <faisal.muhammad2911@gmail.com>
2025-02-21 18:12:24 +0700

    adding line-height as tailwind theme

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,91 @@ const About = () => {
   return (
     <>
       <h1>This is About Page</h1>
-      <p>
+      <p className="mb-16 text-xs font-bold capitalize">
+        (tulis tentang saya)Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Exercitationem, delectus quo neque, repellat laudantium iusto non
+        fugiat vitae architecto facere odit inventore quae impedit fugit.
+        Delectus quae quidem mollitia facere.
+      </p>
+      <p className="mb-16 text-sm font-bold capitalize">
+        (tulis tentang saya)Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Exercitationem, delectus quo neque, repellat laudantium iusto non
+        fugiat vitae architecto facere odit inventore quae impedit fugit.
+        Delectus quae quidem mollitia facere.
+      </p>
+      <p className="mb-16 text-base font-bold capitalize">
+        (tulis tentang saya)Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Exercitationem, delectus quo neque, repellat laudantium iusto non
+        fugiat vitae architecto facere odit inventore quae impedit fugit.
+        Delectus quae quidem mollitia facere.
+      </p>
+      <p className="mb-16 text-lg font-bold capitalize">
+        (tulis tentang saya)Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Exercitationem, delectus quo neque, repellat laudantium iusto non
+        fugiat vitae architecto facere odit inventore quae impedit fugit.
+        Delectus quae quidem mollitia facere.
+      </p>
+      <p className="mb-16 text-xl font-bold capitalize">
+        (tulis tentang saya)Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Exercitationem, delectus quo neque, repellat laudantium iusto non
+        fugiat vitae architecto facere odit inventore quae impedit fugit.
+        Delectus quae quidem mollitia facere.
+      </p>
+      <p className="mb-16 text-2xl font-bold capitalize">
+        (tulis tentang saya)Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Exercitationem, delectus quo neque, repellat laudantium iusto non
+        fugiat vitae architecto facere odit inventore quae impedit fugit.
+        Delectus quae quidem mollitia facere.
+      </p>
+      <p className="mb-16 text-3xl font-bold capitalize">
+        (tulis tentang saya)Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Exercitationem, delectus quo neque, repellat laudantium iusto non
+        fugiat vitae architecto facere odit inventore quae impedit fugit.
+        Delectus quae quidem mollitia facere.
+      </p>
+      <p className="mb-16 text-4xl font-bold capitalize">
+        (tulis tentang saya)Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Exercitationem, delectus quo neque, repellat laudantium iusto non
+        fugiat vitae architecto facere odit inventore quae impedit fugit.
+        Delectus quae quidem mollitia facere.
+      </p>
+      <p className="mb-16 text-5xl font-bold capitalize">
+        (tulis tentang saya)Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Exercitationem, delectus quo neque, repellat laudantium iusto non
+        fugiat vitae architecto facere odit inventore quae impedit fugit.
+        Delectus quae quidem mollitia facere.
+      </p>
+      <p className="mb-16 text-6xl font-bold capitalize">
+        (tulis tentang saya)Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Exercitationem, delectus quo neque, repellat laudantium iusto non
+        fugiat vitae architecto facere odit inventore quae impedit fugit.
+        Delectus quae quidem mollitia facere.
+      </p>
+      <p className="mb-16 text-7xl font-bold capitalize">
+        (tulis tentang saya)Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Exercitationem, delectus quo neque, repellat laudantium iusto non
+        fugiat vitae architecto facere odit inventore quae impedit fugit.
+        Delectus quae quidem mollitia facere.
+      </p>
+      <p className="mb-16 text-8xl font-bold capitalize">
+        (tulis tentang saya)Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Exercitationem, delectus quo neque, repellat laudantium iusto non
+        fugiat vitae architecto facere odit inventore quae impedit fugit.
+        Delectus quae quidem mollitia facere.
+      </p>
+      <p className="mb-16 text-9xl font-bold capitalize">
+        (tulis tentang saya)Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Exercitationem, delectus quo neque, repellat laudantium iusto non
+        fugiat vitae architecto facere odit inventore quae impedit fugit.
+        Delectus quae quidem mollitia facere.
+      </p>
+      <p className="text-10xl mb-16 font-bold capitalize">
+        (tulis tentang saya)Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Exercitationem, delectus quo neque, repellat laudantium iusto non
+        fugiat vitae architecto facere odit inventore quae impedit fugit.
+        Delectus quae quidem mollitia facere.
+      </p>
+      <p className="text-11xl mb-16 font-bold capitalize">
         (tulis tentang saya)Lorem ipsum dolor sit amet consectetur, adipisicing
         elit. Exercitationem, delectus quo neque, repellat laudantium iusto non
         fugiat vitae architecto facere odit inventore quae impedit fugit.
--- src/components/Card/DetailedCard.tsx
+++ src/components/Card/DetailedCard.tsx
@@ -4,7 +4,7 @@ import type { Blog } from "content-collections";
 const DetailedCard = ({ post }: { post: Blog }) => {
   return (
     <div>
-      <h1>{post.title}</h1>
+      <h1 className="font-bold">{post.title}</h1>
       {post.summary ?? <p>{post.summary}</p>}
       <Link href={post.slug}>Read more</Link>
     </div>
