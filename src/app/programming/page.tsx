397e9e7a13e6cccbc3f4154ecc8b26d75d5eb43e
faisalm29 <faisal.muhammad2911@gmail.com>
2025-02-21 21:32:44 +0700

    styling /programming page

--- src/app/programming/page.tsx
+++ src/app/programming/page.tsx
@@ -5,14 +5,19 @@ const ProgrammingPage = () => {
   const sortedPosts = allProgrammings.sort(
     (a, b) =>
       Date.parse(b.publishedDate.toISOString()) -
-      Date.parse(a.publishedDate.toISOString())
+      Date.parse(a.publishedDate.toISOString()),
   );
   return (
-    <>
-      {sortedPosts.map((post, id) => (
-        <DetailedCard key={id} post={post} />
-      ))}
-    </>
+    <div className="mt-24">
+      <h1 className="text-secondary-200 mb-8 font-bold">All Posts</h1>
+      <ul>
+        {sortedPosts.map((post, id) => (
+          <li key={id}>
+            <DetailedCard post={post} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
