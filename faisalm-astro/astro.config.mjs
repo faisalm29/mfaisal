edca519f3469cec7b044a2bd90030730f6dbb99d
faisalm29 <faisal.muhammad2911@gmail.com>
2025-10-10 15:55:15 +0700

    added reading time and mdx layout for general posts

--- faisalm-astro/astro.config.mjs
+++ faisalm-astro/astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
