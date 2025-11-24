d55bd1a5235417f0d8c313eaa85343bb04ec3cad
faisalm29 <faisal.muhammad2911@gmail.com>
2025-11-25 02:01:58 +0700

    fixed local fonts error weight

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,11 +1,11 @@
 
 export default new Map([
 ["src/data/general/mystery-of-time.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fgeneral%2Fmystery-of-time.mdx&astroContentModuleFlag=true")],
-["src/data/general/postingan-ketiga-saya.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fgeneral%2Fpostingan-ketiga-saya.mdx&astroContentModuleFlag=true")],
 ["src/data/general/postingan-kedua-saya.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fgeneral%2Fpostingan-kedua-saya.mdx&astroContentModuleFlag=true")],
 ["src/data/general/postingan-pertama-saya.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fgeneral%2Fpostingan-pertama-saya.mdx&astroContentModuleFlag=true")],
-["src/data/programming/how-i-built-my-blog.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fprogramming%2Fhow-i-built-my-blog.mdx&astroContentModuleFlag=true")],
+["src/data/general/postingan-ketiga-saya.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fgeneral%2Fpostingan-ketiga-saya.mdx&astroContentModuleFlag=true")],
 ["src/data/programming/blockchain-in-2025.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fprogramming%2Fblockchain-in-2025.mdx&astroContentModuleFlag=true")],
+["src/data/programming/how-i-built-my-blog.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fprogramming%2Fhow-i-built-my-blog.mdx&astroContentModuleFlag=true")],
 ["src/data/programming/programming-kedua-saya.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fprogramming%2Fprogramming-kedua-saya.mdx&astroContentModuleFlag=true")],
 ["src/data/programming/programming-ketiga-saya.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fprogramming%2Fprogramming-ketiga-saya.mdx&astroContentModuleFlag=true")],
 ["src/data/programming/programming-pertama-saya.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fprogramming%2Fprogramming-pertama-saya.mdx&astroContentModuleFlag=true")],
